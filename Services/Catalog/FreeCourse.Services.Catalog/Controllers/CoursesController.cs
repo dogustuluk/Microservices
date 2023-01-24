3f9355bc9b659bd8b861bc3eb30d64e69f2dd838
dogustuluk <dogus.tuluk@gmail.com>
2023-01-24 14:35:45 +0300

    created CoursesController and CategoryController

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,66 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        internal CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")] //courses/2 şeklinde url yapısı
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+            /*CreateActionResultInstance
+             * burada generic vermemize gerek yok çünkü base sınıfta parametre olarak generic geçiyoruz yani buraya bir nevi generic verilmiş hali geliyor. bu kod ile if-else kontrolleri ile response'un 200,204,404 gibi kodlarla mı geldiğini kontrol etmemiş oluruz.*/
+
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")] //api/courses/getallbyuserid/5 url yapısı
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserIdAsync(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+            return CreateActionResultInstance(response);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,27 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+    /*ControllerBase
+     * burada Controller sınıfını miras alamayız çünkü aspnetcore'a ait olduğu için ama bu durumu aşmanın bir yolu vardır. burada projeye sağ tıklayıp "edit project file" dedikten sonra elle framework'ü eklememiz gerekiyor. Yazılacak olan kısım --> <ItemGroup>
+		<FrameworkReference Include="Microsoft.AspNetCore.App" />
+		</ItemGroup>
+     */
+    public class CustomBaseController : ControllerBase
+    {
+        /*CreateActionResultInstance
+         * api controller'larında sürekli olarak status code dönmemize gerek kalmadan işlemi daha da sadeleştirebiliriz. Burada response'ın status code'unu alıyoruz bu sayede controller'larda return ok, return bad request, 404 not found gibi dönmemize gerek kalmıyor.
+         */
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
