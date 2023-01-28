a83985544255a370104030ae8202d38a7d20aa09
dogustuluk <dogus.tuluk@gmail.com>
2023-01-28 16:34:13 +0300

    BasketsController

--- Services/Basket/FreeCourse.Services.Basket/Startup.cs
+++ Services/Basket/FreeCourse.Services.Basket/Startup.cs
@@ -34,6 +34,8 @@ namespace FreeCourse.Services.Basket
 
             services.AddScoped<ISharedIdentityService, SharedIdentityService>();
 
+            services.AddScoped<IBasketService, BasketService>();
+
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
 
 
