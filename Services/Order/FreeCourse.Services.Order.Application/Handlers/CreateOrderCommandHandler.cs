da71c90ef694515b52a36fe4c3e0b18b372f78b5
dogustuluk <dogus.tuluk@gmail.com>
2023-02-05 17:22:12 +0300

    update

--- Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -24,7 +24,7 @@ namespace FreeCourse.Services.Order.Application.Handlers
 
         public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
         {
-            var newAddress = new Address(request.AddressDto.Province, request.AddressDto.District, request.AddressDto.Street, request.AddressDto.ZipCode, request.AddressDto.Line);
+            var newAddress = new Address(request.Address.Province, request.Address.District, request.Address.Street, request.Address.ZipCode, request.Address.Line);
 
             //order'a eklemek için order sınfını oluştur. adresi ve orderitem'ı tek başına yükleyemeyiz çünkü aggregate root'umuz order.
             Domain.OrderAggregate.Order newOrder = new Domain.OrderAggregate.Order(request.BuyerId, newAddress);
