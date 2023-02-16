58e63ee124fa71aa8db885d513c1c9d4501c7e33
dogustuluk <dogus.tuluk@gmail.com>
2023-02-16 14:29:11 +0300

    IdentityModel and options pattern(ClientSettings & ServiceApiSettings)

---
+++ Frontends/FreeCourse.Web/Models/ClientSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace FreeCourse.Web.Models
+{
+    public class ClientSettings
+    {
+        public Client WebClient { get; set; }
+        public Client WebMvcClientForUser { get; set; }
+    }
+
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
