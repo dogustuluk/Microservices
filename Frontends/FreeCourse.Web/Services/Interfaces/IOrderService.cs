0778f931f8e9617aeecb5fac0bb2888ecfb5dd6e
dogustuluk <dogus.tuluk@gmail.com>
2023-02-23 19:03:14 +0300

    OrderService

--- Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
+++ Frontends/FreeCourse.Web/Services/Interfaces/IOrderService.cs
@@ -4,6 +4,13 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Web.Services.Interfaces
 {
+    /*senkron haberleşme
+     * Eğer birden fazla mikroserviste senkron bir haberleşme yapmak istersek burada --> SAGA <-- gibi patternleri kullanmamız gerekir. Çünkü birden fazla mikroservice ile haberleşme durumunda distributed transaction'ı yönetmemiz gerekmektedir.
+     * Distributed Transaction --->>> Yani; 3 tane mikroservisle işlem yapıyoruz veri tabanlarında işlem yapmak için, eğer bir tanesinde problem olursa kalan 2 tanede iptal edecek yeni istekler göndermemiz gerekir.
+     */
+    /* reTry mekanizması
+     * Eğer ödeme işleminde bir hata meydana gelirse bir tane reTry mekanizması kurmamız gerekir. Bu mekanizma ile ödeme esnasında bir hata olursa ödeme yapan isteğin birkaç saniye içerisinde tekrardan oluşturulmasıdır. Detaylarını araştır.
+     */
     public interface IOrderService
     {
         /// <summary>
